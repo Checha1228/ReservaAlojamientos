085b5c90fd1de41b7dfef906fad47d0738e2e9e0
Checha1228 <ladycastdg@gmail.com>
2020-06-01 18:11:11 -0500

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+function App() {
+	return (
+		<div>
+			Hello, world!
+			
+		</div>
+		
+	);
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('app')
+);
---
+++ scripts/components/rsrvbutton/rsrvbutton.js
@@ -0,0 +1,10 @@
+class RsrvButton extends React.Component {
+	render () {
+		return (
+			<div>
+				Hello, world! 2
+			</div>
+		);
+	}
+};
+
